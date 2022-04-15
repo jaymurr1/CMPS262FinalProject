1bee2391e8a4a15b3eee05d55ac2a6cd5b889fc2
Jarrell Murray-Sinicki <jmurra1@pointpark.edu>
2022-04-15 17:22:10 -0400

    initial upload, contains basic html pages and style sheet still need to upload finished api stuff

---
+++ script.js
@@ -0,0 +1,12 @@
+//TODO SOON
+
+
+
+
+
+
+
+
+//implement search for video
+// either search  using video name or implement a way to search using whatever the active video in the gallery is
+//might have to store the video name in a seperate data structure
